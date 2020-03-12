8be5ced21c0cf8c4c97fb6c03c87a6593809df8e
ubunto <JavaCoffeeBean50@gmail.com>
2020-03-12 14:18:32 -0500

    learned about data types in kotlin

--- src/KotlinPractice.kt
+++ src/KotlinPractice.kt
@@ -1,12 +1,13 @@
 fun main() {
-    println("I Love Cash")
-    carSound()
+    val fName: String = "Kingsley"
+    var temp: Int = 98
+    val height: Float = 5.5f
+    var gpa: Double = 3.1
+    val answerChoice: Char = 'c'
+    println("This is Kotlin.")
+    favCar()
 }
 
-fun carSound() {
-    println("vrooom VROOOOOOM")
-}
-
-fun carBrand() {
-    println("Toyota")
+fun favCar() {
+    println("Audi is my favorite car brand")
 }
